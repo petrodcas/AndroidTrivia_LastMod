33767c5d7d2e26113cbe4acefe56084956377960
Peter Rodríguez Casado <petrodcas@gmail.com>
2022-01-08 18:29:15 +0100

    Se ha creado el nuevo fragmento con su apariencia vertical y se han añadido sus strings a los archivos strings.xml.
    Aún no se encuentran vinculados al menú deslizante, pero la app sigue funcionando.

---
+++ app/src/main/java/com/example/android/navigation/questionAdder/QuestionAdderFragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.navigation.questionAdder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.android.navigation.R
+import com.example.android.navigation.databinding.FragmentQuestionAdderBinding
+
+
+class QuestionAdderFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentQuestionAdderBinding>(inflater, R.layout.fragment_question_adder, container, false)
+        //TODO: establece un listener en el botón para volver al TitleFragment
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
